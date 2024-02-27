10028b2be5f9f71267c41efd99258a8a84fe0eef
PauloAndre05 <pauloandrepaulinho05@gmail.com>
2024-02-27 22:06:25 +0000

    Finish the first part of responsive design(Pequenos ajustes)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,11 +8,11 @@ export function Banner () {
                 className="absolute inset-0 w-full h-full bg-no-repeat object-cover opacity-5 max-sm:opacity-10" 
                 alt="Imagem de Fundo"
             />
-            <div className="flex flex-col gap-4 z-10 max-sm:m-auto max-sm:text-center">
+            <div className="flex flex-col gap-4 z-10 ">
                 <h1 className="text-6xl font-bold text-third animate-fadeIn max-sm:text-5xl">Front-End Developer</h1>
-                <p className="text-xl animate-fadeIn">Paulo Samuel António André</p>
-                <p className="text-xl animate-fadeIn">HTML * CSS * TYPESCRIPT</p>
-                <button className='w-40 p-2 rounded-full bg-four hover:opacity-50 max-sm:m-auto animate-fadeIn'>Download CV</button>
+                <p className="animate-fadeIn">Paulo Samuel António André</p>
+                <p className="animate-fadeIn">HTML * CSS * TYPESCRIPT</p>
+                <button className='w-40 p-2 rounded-full bg-four hover:opacity-50 animate-fadeIn'>Download CV</button>
             </div>
 
             <div className='w-[30rem] h-[30rem] rounded-[50%]  max-sm:hidden'>
